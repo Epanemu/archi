cbeb330cb0c20c05b861245026986e9467b700d3
Jiri Nemecek <nemecek.jiri@epita.fr>
2018-12-04 12:47:04 +0100

    practicals 10 done

--- space_invaders.asm
+++ space_invaders.asm
@@ -6,6 +6,9 @@ VIDEO_HEIGHT        equ		320							   ; Height in pixels
 VIDEO_SIZE          equ		(VIDEO_WIDTH*VIDEO_HEIGHT/8)   ; Size in bytes
 BYTE_PER_LINE     	equ		(VIDEO_WIDTH/8)
 
+WIDTH               equ		0
+HEIGHT              equ		2
+MATRIX              equ		4
 ;----------------------------  Init  -------------------------------
 
 					org		$0
@@ -16,7 +19,68 @@ vector_001          dc.l    Main
 
 					org		$500
 					
-Main                move.w	#2,d0
+Main                lea     InvaderA_Bitmap,a0
+					lea     VIDEO_START+14+100*BYTE_PER_LINE,a1
+					jsr     CopyBitmap
+					
+					lea     InvaderB_Bitmap,a0
+					lea     VIDEO_START+28+100*BYTE_PER_LINE,a1
+					jsr     CopyBitmap
+					
+					lea     InvaderC_Bitmap,a0
+					lea     VIDEO_START+42+100*BYTE_PER_LINE,a1
+					jsr     CopyBitmap
+					
+					lea     Ship_Bitmap,a0
+					lea     VIDEO_START+28+200*BYTE_PER_LINE,a1
+					jsr     CopyBitmap
+					
+					illegal
+
+
+					move.l	#VIDEO_START,a1
+					
+					move.l	#InvaderA_Bitmap,a0
+					
+					jsr		CopyBitmap
+					
+					moveq.l	#$0,d0
+					jsr     FillScreen
+					
+					move.l	#InvaderB_Bitmap,a0
+					
+					jsr		CopyBitmap
+					
+					moveq.l	#$0,d0
+					jsr     FillScreen
+					
+					move.l	#InvaderC_Bitmap,a0
+					
+					jsr		CopyBitmap
+					
+					moveq.l	#$0,d0
+					jsr     FillScreen
+					
+					move.l	#Ship_Bitmap,a0
+					
+					jsr		CopyBitmap
+					
+					illegal
+					
+					move.b	#16,d1
+\rows				clr		d0
+\columns			move.b	(a1)+,0(a0,d0.l)
+					addi.b	#1,d0
+					cmp.b	#3,d0
+					bne		\columns
+					adda.l	#BYTE_PER_LINE,a0
+					subi.b	#1,d1
+					bne		\rows
+					
+	
+					illegal
+
+					move.w	#2,d0
 
 \loop               jsr     WhiteSquare
 
@@ -161,3 +225,107 @@ WhiteLine			movem.l a0/d0,-(a7)	;len in d0 (in bytes) start in a0
 					
 					movem.l	(a7)+,a0/d0
 					rts	
+
+PixelToByte			move.l	d2,-(a7)
+					move.l	d3,d2
+					clr		d3
+\loop				subi.w	#8,d2
+					addi.w	#1,d3
+					tst.w	d2
+					bgt		\loop
+					
+					move.l	(a7)+,d2
+					rts
+					
+CopyLine			movem.l	a1/d3,-(a7)
+\loop				move.b	(a0)+,(a1)+
+					subi.w	#1,d3
+					bne		\loop
+					
+					movem.l	(a7)+,a1/d3
+					rts
+
+CopyBitmap			movem.l	a0/a1/d0/d3,-(a7)
+					move.w	WIDTH(a0),d3
+					move.w	HEIGHT(a0),d0
+					adda.l	#MATRIX,a0
+					jsr		PixelToByte
+\loop				jsr		CopyLine
+					adda.l	#BYTE_PER_LINE,a1
+					subi.w	#1,d0
+					bne		\loop
+					
+					movem.l	(a7)+,a0/a1/d0/d3
+					rts
+			
+;------------------------------ DATA -------------------------------		
+
+InvaderA_Bitmap 	dc.w	24,16
+					dc.b    %00000000,%11111111,%00000000
+					dc.b    %00000000,%11111111,%00000000
+					dc.b    %00111111,%11111111,%11111100
+					dc.b    %00111111,%11111111,%11111100
+					dc.b    %11111111,%11111111,%11111111
+					dc.b    %11111111,%11111111,%11111111
+					dc.b    %11111100,%00111100,%00111111
+					dc.b    %11111100,%00111100,%00111111
+					dc.b    %11111111,%11111111,%11111111
+					dc.b    %11111111,%11111111,%11111111
+					dc.b    %00000011,%11000011,%11000000
+					dc.b    %00001111,%11000011,%11000000
+					dc.b    %00001111,%00111100,%11110000
+					dc.b    %00001111,%00111100,%11110000
+					dc.b    %11110000,%00000000,%00001111
+					dc.b    %11110000,%00000000,%00001111
+
+InvaderB_Bitmap 	dc.w	22,16
+					dc.b    %00001100,%00000000,%11000000
+					dc.b    %00001100,%00000000,%11000000
+					dc.b    %00000011,%00000011,%00000000
+					dc.b    %00000011,%00000011,%00000000
+					dc.b    %00001111,%11111111,%11000000
+					dc.b    %00001111,%11111111,%11000000
+					dc.b    %00001100,%11111100,%11000000
+					dc.b    %00001100,%11111100,%11000000
+					dc.b    %00111111,%11111111,%11110000
+					dc.b    %00111111,%11111111,%11110000
+					dc.b    %11001111,%11111111,%11001100
+					dc.b    %11001111,%11111111,%11001100
+					dc.b    %11001100,%00000000,%11001100
+					dc.b    %11001100,%00000000,%11001100
+					dc.b    %00000011,%11001111,%00000000
+					dc.b    %00000011,%11001111,%00000000
+
+InvaderC_Bitmap 	dc.w	16,16
+					dc.b    %00000011,%11000000
+					dc.b    %00000011,%11000000
+					dc.b    %00001111,%11110000
+					dc.b    %00001111,%11110000
+					dc.b    %00111111,%11111100
+					dc.b    %00111111,%11111100
+					dc.b    %11110011,%11001111
+					dc.b    %11110011,%11001111
+					dc.b    %11111111,%11111111
+					dc.b    %11111111,%11111111
+					dc.b    %00110011,%11001100
+					dc.b    %00110011,%11001100
+					dc.b    %11000000,%00000011
+					dc.b    %11000000,%00000011
+					dc.b    %00110000,%00001100
+					dc.b    %00110000,%00001100
+					
+Ship_Bitmap		 	dc.w	24,14
+					dc.b    %00000000,%00011000,%00000000
+					dc.b    %00000000,%00011000,%00000000
+					dc.b    %00000000,%01111110,%00000000
+					dc.b    %00000000,%01111110,%00000000
+					dc.b    %00000000,%01111110,%00000000
+					dc.b    %00000000,%01111110,%00000000
+					dc.b    %00111111,%11111111,%11111100
+					dc.b    %00111111,%11111111,%11111100
+					dc.b    %11111111,%11111111,%11111111
+					dc.b    %11111111,%11111111,%11111111
+					dc.b    %11111111,%11111111,%11111111
+					dc.b    %11111111,%11111111,%11111111
+					dc.b    %11111111,%11111111,%11111111
+					dc.b    %11111111,%11111111,%11111111
